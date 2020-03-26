135f7489b16a8389316256a4435c1d6c6df51bbe
abdou10144 <52550867+abdou10144@users.noreply.github.com>
2020-03-26 01:56:41 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("692535721281454132")
+setInterval(function() {
+channel.send(`nbghik wlh ghir nbghik`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
